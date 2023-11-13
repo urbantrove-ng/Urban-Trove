4d1a6dca7ef64006745947347806d8cd1e7f9ff3
humblesmarts <humblesmarts11@gmail.com>
2023-11-14 00:29:49 +0100

    ADD: Social login on the signup page

--- client/src/components/Account/SocialLogin.jsx
+++ client/src/components/Account/SocialLogin.jsx
@@ -1,51 +1,30 @@
 import React from "react";
+import { FaApple, FaFacebookF } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 export default function SocialLogin() {
   return (
-    <div>
-      <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
-        <div className="flex flex-wrap text-center justify-center"></div>
-        <div className="flex flex-wrap mt-12 justify-center">
-          <div className="w-full lg:w-3/12 px-4 text-center">
-            <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-              <FaMedal />{" "}
-            </div>
-            <h6 className="text-xl mt-5 font-semibold text-white">
-              Mobile Data & Airtime
-            </h6>
-            <p className="mt-2 mb-4 text-gray-500">
-              Never run out of data or airtime again. Top up your mobile phone
-              with just a few clicks, and enjoy uninterrupted connectivity. We
-              offer competitive rates and support for all major networks.
-            </p>
-          </div>
-          <div className="w-full lg:w-3/12 px-4 text-center">
-            <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-              <FaPoll />{" "}
-            </div>
-            <h5 className="text-xl mt-5 font-semibold text-white">
-              DStv & GOtv Subscriptions
-            </h5>
-            <p className="mt-2 mb-4 text-gray-500">
-              Enjoy your favorite TV shows and sports events without the hassle.
-              Subscribe to DStv and GOtv packages easily and cost-effectively
-              through our platform.
-            </p>
-          </div>
-          <div className="w-full lg:w-3/12 px-4 text-center">
-            <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-              <FaLightbulb />{" "}
-            </div>
-            <h5 className="text-xl mt-5 font-semibold text-white">
-              Electricity Bills
-            </h5>
-            <p className="mt-2 mb-4 text-gray-500">
-              Paying your electricity bills has never been easier. Say goodbye
-              to long queues and late fees. Pay your bills conveniently with us.
-            </p>
-          </div>
-        </div>
+    <>
+      <div className=" mb-1 text-4xl ">
+        <button
+          className="bg-white text-black-400 shadow-lg font-normal  square-full mr-4 p-3"
+          type="button"
+        >
+          <FaApple />{" "}
+        </button>
+        <button
+          className="bg-white text-blue-600 shadow-lg font-normal h-13 w-13 items-center justify-center align-center square-full  mr-2 p-3"
+          type="button"
+        >
+          <FaFacebookF />{" "}
+        </button>
+        <button
+          className="bg-white shadow-lg font-normal square-full  mr-2 p-3"
+          type="button"
+        >
+          <FcGoogle />{" "}
+        </button>
       </div>
-    </div>
+    </>
   );
 }
--- client/src/pages/Auth/SignupPage.jsx
+++ client/src/pages/Auth/SignupPage.jsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/Account/Footer";
 import Header from "../../components/Account/Header";
 import Signup from "../../components/Account/SignupForm";
+import SocialLogin from "../../components/Account/SocialLogin";
 
 export default function SignupPage() {
   return (
     <>
       <Header heading="Uncover Urban Gems with Urban Trove" />
       <Signup />
+      <SocialLogin />
       <Footer
         paragraph="Already have an account? "
         linkName="Login"
