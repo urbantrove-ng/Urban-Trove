ee9553a5c4651b7e2ea99d48e72fb5558e256ff2
humblesmarts <humblesmarts11@gmail.com>
2023-11-18 09:58:44 +0100

    Fix: Logo but logo too small

--- client/src/components/Account/FormHeader.jsx
+++ client/src/components/Account/FormHeader.jsx
@@ -1,16 +1,14 @@
+import logo from "../../assets/image/logo.png";
+
 export default function Header({ heading }) {
   return (
     <div className="mb-1">
       <div className="flex justify-center">
-        <img
-          alt=""
-          className="h-14 w-14"
-          src="https://images.unsplash.com/photo-1573804633927-bfcbcd909acd?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
+        <img alt="logo" className="h-20 w-21" src={logo} />
       </div>
-      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      {/* <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
         {heading}
-      </h2>
+      </h2> */}
     </div>
   );
 }
