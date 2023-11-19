0b838df7333140917787640e074f8f012b6823de
Prince Ekejiuba <64290248+ekoxyprince@users.noreply.github.com>
2023-11-18 19:42:27 -0800

    Signin endpoint completed

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -35,12 +35,12 @@ exports.signin = (req,res,next)=>{
   User.findOne({email:email})
   .then(user=>{
    if(!user){
-    return res.status(400).json({success:false,body:{status:400,title:'Authentication Error',data:[{path:'email',msg:'Incorrect email address'}]}})
+    return res.status(400).json({success:false,body:{status:400,title:'Authentication Error',data:[{path:'email',msg:'Incorrect email address',value:email,location:'body',type:'field'}]}})
    }
    return bcrypt.compare(password,user.password)
    .then(doMatch=>{
     if(!doMatch){
-    return res.status(400).json({success:false,body:{status:400,title:'Authentication Error',data:[{path:'password',msg:'Incorrect email address'}]}})  
+    return res.status(400).json({success:false,body:{status:400,title:'Authentication Error',data:[{path:'password',msg:'Incorrect paswsord',value:password,location:'body',type:'field'}]}})  
     }
     const token = jwt.sign({_id:user._id},jwt_secret,{expiresIn:jwt_expires})
     res.status(200).json({success:true,body:{status:200,title:'Response Success',data:{...user,accessToken:token,msg:'User Logged in successfully'}}})
