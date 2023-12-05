ba01875c86925492e17858b62a0d52b2765ccb29
Prince Ekejiuba <64290248+ekoxyprince@users.noreply.github.com>
2023-12-05 03:42:27 -0800

    enabled cors using cors package

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const path = require('path');
-const rootRoutes = require('./routes/root');
+const rootRoutes = require('./routes/index');
 const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
 const compression = require('compression')
 const helmet = require('helmet')
 const logger = require('morgan')
+const rateLimit = require('express-rate-limit')
+const cors = require ('cors');
+const { allowedOrigin } = require('./config');
 
 const app = express();
 const limiter = rateLimit({
@@ -17,14 +20,6 @@ const limiter = rateLimit({
     legacyHeaders:false
 })
 
-// Enable CORS for all routes
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(bodyParser.json());
@@ -32,6 +27,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(compression())
 app.use(helmet())
 app.use(logger('dev'))
+app.use(cors({origin:allowedOrigin,
+optionsSuccessStatus:200}))
 
 app.use('/api/v1',rootRoutes);
 app.use('/api/v1/auth',authRoutes);
